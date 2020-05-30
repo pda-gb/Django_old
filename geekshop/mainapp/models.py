e2a66c6deca89d2e569ded58b95caab2955bbbcf
Denis <pda.prostor@gmail.com>
2020-05-30 21:47:35 +0300

    ДЗ_3 Первичное создание модели, первая миграция

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
+
 class ProductCategory(models.Model):
+    name = models.CharField(verbose_name="имя", max_length=64, unique=True)
+    description = models.TextField(verbose_name="описание", blank=True)
+
+    def __str__(self):
+        return self.name
+
 
