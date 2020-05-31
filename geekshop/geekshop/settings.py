cf56199e149d160a8824dea9574d88c43d75ae78
Denis <pda.prostor@gmail.com>
2020-06-01 01:31:28 +0300

    4. Создать суперпользователя. Настроить админку и поработать в ней. +

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -119,3 +119,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# admin
+# qwe
+# qwe@mail.cc
--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import ProductCategory, Product
+#  регистрируем в админке
+admin.site.register(ProductCategory)
+admin.site.register(Product)
