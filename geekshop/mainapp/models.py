76f94a00567e888b83bbbac0ea4ce9639e01fecf
Denis <pda.prostor@gmail.com>
2020-05-31 23:04:43 +0300

    3. Поработать с моделями в консоли. в консоли: python manage.py shell from mainapp.models import ProductCategory catecory_2 = ProductCategory(name='Стулья') catecory_2.save() >>> all_cat = ProductCategory.objects.all() >>> all_cat <QuerySet [<ProductCategory: Стулья>]> prod_cat_1 = all_cat[0] prod_cat_1.description = 'отличные стулья'

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -9,3 +9,11 @@ class ProductCategory(models.Model):
         return self.name
 
 
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя продукта', max_length=128)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    short_desc = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
+    description = models.TextField(verbose_name='описание продукта', blank=True)
+    price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
